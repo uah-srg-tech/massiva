ad3a1629c3c17016f9268e2aa59d67aa34e367b3
DESKTOP-8QUK7NG\Aaron <aaron.montalvo@uah.es>
2024-05-06 19:23:51 +0200

    Added back netbeans project files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,59 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=Debug
+CND_ARTIFACT_NAME_Debug=massiva
+CND_ARTIFACT_PATH_Debug=Debug/massiva
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug=massiva.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/massiva.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=Release
+CND_ARTIFACT_NAME_Release=massiva
+CND_ARTIFACT_PATH_Release=Release/massiva
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=massiva.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/massiva.tar
+# Release_no_spw configuration
+CND_PLATFORM_Release_no_spw=MinGW-Windows
+CND_ARTIFACT_DIR_Release_no_spw=Release_no_spw
+CND_ARTIFACT_NAME_Release_no_spw=massiva_no_spw
+CND_ARTIFACT_PATH_Release_no_spw=Release_no_spw/massiva_no_spw
+CND_PACKAGE_DIR_Release_no_spw=dist/Release_no_spw/MinGW-Windows/package
+CND_PACKAGE_NAME_Release_no_spw=massiva.tar
+CND_PACKAGE_PATH_Release_no_spw=dist/Release_no_spw/MinGW-Windows/package/massiva.tar
+# DebugLinux configuration
+CND_PLATFORM_DebugLinux=GNU-Linux
+CND_ARTIFACT_DIR_DebugLinux=DebugLinux
+CND_ARTIFACT_NAME_DebugLinux=massiva
+CND_ARTIFACT_PATH_DebugLinux=DebugLinux/massiva
+CND_PACKAGE_DIR_DebugLinux=dist/DebugLinux/GNU-Linux/package
+CND_PACKAGE_NAME_DebugLinux=massiva.tar
+CND_PACKAGE_PATH_DebugLinux=dist/DebugLinux/GNU-Linux/package/massiva.tar
+# ReleaseLinux configuration
+CND_PLATFORM_ReleaseLinux=GNU-Linux
+CND_ARTIFACT_DIR_ReleaseLinux=ReleaseLinux
+CND_ARTIFACT_NAME_ReleaseLinux=massiva
+CND_ARTIFACT_PATH_ReleaseLinux=ReleaseLinux/massiva
+CND_PACKAGE_DIR_ReleaseLinux=dist/ReleaseLinux/GNU-Linux/package
+CND_PACKAGE_NAME_ReleaseLinux=massiva.tar
+CND_PACKAGE_PATH_ReleaseLinux=dist/ReleaseLinux/GNU-Linux/package/massiva.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ nbproject/private/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+# Debug configuration
+# Release configuration
+# Release_no_spw configuration
+# DebugLinux configuration
+# ReleaseLinux configuration
